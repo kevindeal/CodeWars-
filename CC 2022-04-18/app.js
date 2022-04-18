099c7b0fbfabc4b9509ce0d975213d43d1d2c5b9
Kevin Deal <kevin@superfriend.ly>
2022-04-18 16:32:25 -0400

    add code

---
+++ CC 2022-04-18/app.js
@@ -0,0 +1,14 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// P: make sure your return a string (''); 
+
+// R: write a function that returns a string that includes var name. 
+
+// E: N/a
+
+function greeting(name) {
+  const greeting = `Hello, ${name} how are you doing today?`;
+  return greeting;
+}
